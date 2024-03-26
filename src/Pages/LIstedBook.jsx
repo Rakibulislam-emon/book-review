6f6eb8fd7587e80db86d57b2ac8ce3f34849a618
Rakibulislam-emon <rakibulislamemon60>
2024-03-26 23:51:03 +0600

    files added

---
+++ src/Pages/LIstedBook.jsx
@@ -0,0 +1,16 @@
+import ReadBooks from "./ReadBooks";
+
+
+
+export default function LIstedBook() {
+  return (
+    <div className="">
+      <div >
+        <h1 className='text-[40px] max-w-[90%] m-auto rounded-xl p-8 bg-[#1313130D] border font-bold text-center mt-4 mb-8'>Books</h1>
+      </div>
+   
+      {/* <Outlet></Outlet> */}
+      <ReadBooks></ReadBooks>
+    </div>
+  )
+}
